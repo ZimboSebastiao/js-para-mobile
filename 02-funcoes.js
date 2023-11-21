6be6c8265da2d1ef5f5c0757ab4e11d60ddc789b
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-11-21 09:42:06 -0300

    Update 02-funcoes.js

---
+++ 02-funcoes.js
@@ -0,0 +1,11 @@
+
+// Formas tradicionais
+
+
+// Sintaxe anônima associada à variável/constante.
+// OBS: cuidado com a ordem, já que nesta sintaxe a funçao primeiro precisa ser declarada para somente depois ser chamada/executada
+const exemplo1 = function () {
+    console.log("Função anônima");
+}
+
+exemplo1();
