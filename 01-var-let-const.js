c8553769948d97cc0934f2c203a57c907ef62dcc
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-11-21 09:28:15 -0300

    Update 01-var-let-const.js

--- 01-var-let-const.js
+++ 01-var-let-const.js
@@ -1,14 +1,27 @@
 var idade = 18; // ESCOPO GLOBAL
 
 if (idade >= 18) {
-    let mensagem = "É adulto"; // ESCOPO DE BLOCO
+    // let mensagem = "É adulto"; // ESCOPO DE BLOCO
 
 } else {
-    let mensagem = "É menor"; // ESCOPO DE BLOCO
+    // let mensagem = "É menor"; // ESCOPO DE BLOCO
 
 }
 
 console.log(idade);
 
 // Dará erro porque mensagem só existe no bloco if/else
-console.log(mensagem);
+// console.log(mensagem);
+
+const meuNome = "Zimbo";
+console.log(meuNome);
+
+
+
+// Reatribuição NÃO é possível em constantes
+// meuNome = "Zimbo Al";
+console.log(meuNome);
+
+// Constantes sempre são declaradas com algum valor
+// e este valo não pode ser Modificado
+const escola = "Senac";
