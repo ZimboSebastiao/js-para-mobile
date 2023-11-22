a39f1d4fa03a498cdf197973e8dd37e24e896007
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-11-22 11:52:25 -0300

    Update 05-operador-spread.js

--- 05-operador-spread.js
+++ 05-operador-spread.js
@@ -1,8 +1,29 @@
 // spread -> espalhar
 // Copiar elementos/dados de um array/objeto para dentro de outro array/elemento
 
+
+// Spread com Arrays
 const bandas = ['Pink Floyd', ' Rush', 'Yes'];
 const maisBandas = [ ...bandas, "Slayer", "Nightwist"];
 
 console.log(bandas);
 console.log(maisBandas);
+
+// Spread com Objetos
+
+const cliente = {
+    nome: "Seu Madruga",
+    cidade: "São Paulo",
+    idade: 55
+}
+
+const novosDados = {
+    ...cliente, //copiando as props. do cliente pra cá
+    // ...maisBandas,
+    estado: "SP",
+    pedido: "76873RDF",
+    total: 50098,
+    idade: 60 // sobreescrevendo valores de propriedades
+}
+
+console.log(novosDados);
