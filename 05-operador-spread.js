670c902f212be050dcbbc6b67e433256a2bbe71a
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-11-22 11:44:14 -0300

    Operador spread com array

---
+++ 05-operador-spread.js
@@ -0,0 +1,8 @@
+// spread -> espalhar
+// Copiar elementos/dados de um array/objeto para dentro de outro array/elemento
+
+const bandas = ['Pink Floyd', ' Rush', 'Yes'];
+const maisBandas = [ ...bandas, "Slayer", "Nightwist"];
+
+console.log(bandas);
+console.log(maisBandas);
