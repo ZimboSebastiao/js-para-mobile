bd3de921637f753a0fa5b097d215ef0dfe9f0c6e
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-11-22 08:46:43 -0300

    Update template-literal.js

--- template-literal.js
+++ template-literal.js
@@ -4,3 +4,12 @@ const sobrenome = "Sebastião";
 let idade = 25;
 let cidade = "São Paulo";
 let estado = "SP";
+
+/* Concatenação tradicional usando operador +
+
+Meu nome é Zimbo Sebastião, tenho 25 anos e moro atualmente na cidade de São Paulo - SP
+*/
+
+let mensagem = "Meu nome é "+nome+ " "+sobrenome+ ", tenho "+idade+" anos e moro atualmente na cidade de "+cidade+" - "+estado;
+
+console.log(mensagem);
