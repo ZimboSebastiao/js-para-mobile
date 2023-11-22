18f928462d0481b9209d1ba87632d9f7bb3df644
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-11-22 09:35:24 -0300

    export

--- 04-codigo-modular.js
+++ 04-codigo-modular.js
@@ -1,3 +1,9 @@
-import pessoa from "./modulos/dados.js";
+import {pessoa, livro} from "./modulos/dados.js";
 
 console.log(pessoa);
+console.log(pessoa.nome);
+
+for (const prop in pessoa) {
+    console.log(pessoa[prop]);
+    
+}
--- modulos/dados.js
+++ modulos/dados.js
@@ -4,4 +4,13 @@ const pessoa = {
     cidade: 'São paulo'
 }
 
-export default pessoa;
+const livro = {
+    titulo: 'O Senhor dos Anéis',
+    volume: 'As Duas Torres',
+    ano: 1954
+}
+
+
+// export default pessoa; Se for exportação de um recursos
+
+export {pessoa, livro}; // Se for exportação de vários recursos
