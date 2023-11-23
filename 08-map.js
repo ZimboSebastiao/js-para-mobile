af1392b6ad16f63c8d3cb0d248fe5f77fc1944fe
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-11-23 10:57:35 -0300

    Update 08-map.js

--- 08-map.js
+++ 08-map.js
@@ -1,6 +1,7 @@
+import cursos from "./modulos/cursos.js";
 /*
 map (mapear, mapeia - transformar em outra coisa)
-Passa por elementos de um array e realiza operações em cada um deles através de uma função, gerando um  novo array.
+Passa por elementos de um array e realiza operações em cada um deles através de uma função (callback), gerando um  novo array.
 */
 
 // Exemplo 1
@@ -9,3 +10,11 @@ console.log(numeros);
 const numerosDobrados = numeros.map(numero => numero * 2);
 
 console.log(numerosDobrados);
+
+console.log("\n");
+console.log(cursos);
+
+// Exemplo 2: Gerar um novo array apenas com os nomes dos cursos
+const titulos = cursos.map(curso => curso.titulo);
+
+console.log(titulos);
