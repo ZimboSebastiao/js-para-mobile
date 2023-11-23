457351db89fbaa9814d9849a2026ec2fbdd65298
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-11-23 09:01:21 -0300

    Create 07-desestruturacao.js

---
+++ 07-desestruturacao.js
@@ -0,0 +1,15 @@
+
+/* Destructuring ou Desestruturação
+Extrair os dados de arrays e objetos para variáveis/constantes individuais
+
+
+
+/* Destructuring em arrays */
+
+const alunas = ["Tanaka", "Zimbo", "Melissa"];
+
+const [tanaka, zimbo, melissa] = alunas;
+
+console.log(tanaka);
+console.log(zimbo);
+console.log(melissa);
