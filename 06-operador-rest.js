4e019dba5aeafc6807f14f3f71e3cd14a4ad77ec
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-11-23 08:41:11 -0300

    Create 06-operador-rest.js

---
+++ 06-operador-rest.js
@@ -0,0 +1,14 @@
+
+/*Conceito
+Usando o ... como "rest operator"
+podemos mesclar
+*/ 
+const cientistas = ["Tesla", "Einstein", "Newton", "Darwin"];
+const artistas = ["Tayc", "Ya Levis", "Dadju", "Paulo Flores", "Yola Semedo", "CEF"];
+                    // rest
+const classificar = (...parametro) => {
+    return parametro.sort();
+};
+
+console.log(classificar(...cientistas)); // Spread
+console.log(classificar(...artistas)); // Spread
