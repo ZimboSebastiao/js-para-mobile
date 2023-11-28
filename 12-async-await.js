0739c0706b9d211c6cdf82a23da18dfdffac76a2
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-11-28 10:57:32 -0300

    exercicio

--- 12-async-await.js
+++ 12-async-await.js
@@ -5,7 +5,7 @@ usando fetch e a sintaxe
 async/await atráves de função(obrigatório)
 */
 
-const apiUrl = `https://jsonplaceholder.typicode.com/photos`;
+const apiUrl = `https://jsonplaceholder.typicode.com/users`;
 async function acessaApi() {
     try {
         const resposta = await fetch(apiUrl);
@@ -16,9 +16,19 @@ async function acessaApi() {
         }
         const dados = await resposta.json();
         console.log(dados);
+
+        const lista = document.querySelector("div")
+        for(const dado of dados){
+
+                let item = document.createElement("p");
+                item.innerHTML = `Nome completo: ${dado.name}  ${dado. username}`;
+                lista.appendChild(item);
+            
+        }
     } catch (error) {
         console.error("Erro: " +error.message);
     }    
 }
 
 acessaApi();
+
