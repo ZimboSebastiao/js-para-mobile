bcbeff11a1bd02e793b9f8fbd95ca5d9eb837520
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-11-28 09:12:19 -0300

    agrupamento em funçao

--- 11-promises-com-fetch.js
+++ 11-promises-com-fetch.js
@@ -5,22 +5,30 @@ carregamento de dados usando fetch/then/catch */
 
 const apiUrl = `https://jsonplaceholder.typicode.com/users/3`;
 
-// Conecte/Acesse...
-fetch(apiUrl)
+function acessaApi() {
+    
+    // Conecte/Acesse...
+    fetch(apiUrl)
 
-// ... e então capture a resposta da API no formato JSON
-.then(resposta => {
-    // se a resposta da requisição não for bem sucedida
-    if (!resposta.ok) {
-        throw new Error (
-            `Erro na requisição: ${resposta.status} - ${resposta.statusText}`
-        )
-    }
-    return resposta.json()
-})
+    // ... e então capture a resposta da API no formato JSON
+    .then(resposta => {
+        // se a resposta da requisição não for bem sucedida
+        if (!resposta.ok) {
+            throw new Error (
+                `Erro na requisição: ${resposta.status} - ${resposta.statusText}`
+            )
+        }
+        return resposta.json()
+    })
 
-// ... e então capture os dados contidos na resposta
-.then( dados => console.log(dados) )
+    // ... e então capture os dados contidos na resposta
+    .then( dados => console.log(dados) )
 
-// em caso de erro (rede, acesso. no json, gerais)
-.catch(error => console.error("Erro na operação: "+error.message));
+    // em caso de erro (rede, acesso. no json, gerais)
+    .catch(error => console.error("Erro na operação: "+error.message));
+
+
+
+}
+
+acessaApi();
